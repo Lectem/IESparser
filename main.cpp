f26fa8d4c42b127ffb4c78464782307c65f86e61
Lectem <lectem@gmail.com>
2015-02-08 17:21:41 +0100

    recursive parsing

--- main.cpp
+++ main.cpp
@@ -3,10 +3,63 @@
 #include "dirent.h"
 #include "database.h"
 
-
+#include <queue>
 #include <sys/stat.h>
 #include <unistd.h>
 using namespace std;
+
+
+void parseDirectoryRec(string dir)
+{
+    queue<string> dirs;
+    dirs.push(dir);
+    while(!dirs.empty())
+    {
+        string curdir = dirs.front();
+        dirs.pop();
+        DIR * dir =opendir(curdir.c_str());
+        if(dir)
+        {
+            struct dirent * entry = readdir(dir);
+            cout << "Parsing files of directory " << curdir << endl;
+            while(entry)
+            {
+                if(DT_REG == entry->d_type)
+                {
+                    string filename (entry->d_name);
+                    if(!filename.substr(filename.length()-4, filename.length()).compare(".ies")) {
+                        cout << entry->d_name << endl;
+                        string s(entry->d_name);
+                        IES::IES ies((curdir + ("/" + s)).c_str());
+                        rmdir("output/");
+                        mkdir("output/");
+                        ies.toSQL("output/" + string(entry->d_name).substr(0, string(entry->d_name).length() - 4)+ ".sql", Database::getAssociatedTable(entry->d_name));
+                        ies.toXML(curdir+"/" + string(entry->d_name).substr(0, string(entry->d_name).length() - 4) + ".xml");
+                    }
+                    else cout << "ignoring " << filename << endl;
+                }
+                else if(DT_DIR == entry->d_type && entry->d_name[0] != '.')
+                {
+                    dirs.push(string(curdir)+"/" +entry->d_name);
+                }
+                else
+                {
+                    cout << "ignoring non regular file " << entry->d_name << endl;
+                }
+                entry = readdir(dir);
+
+            }
+
+            closedir(dir);
+        }
+        else {
+            cout << "can't open dir "<< curdir<<":";
+            perror("");
+        }
+    }
+}
+
+
 
 int main(int argc, char * argv[]) {
     if(argc>2)
@@ -15,41 +68,7 @@ int main(int argc, char * argv[]) {
         stat(argv[1],&buf);
         if(S_ISDIR(buf.st_mode))
         {
-            DIR * dir =opendir(argv[1]);
-            if(dir)
-            {
-                struct dirent * entry = readdir(dir);
-                cout << "Parsing files of directory " << argv[1] << endl;
-                while(entry)
-                {
-                    if(DT_REG == entry->d_type)
-                    {
-                        string filename (entry->d_name);
-                        if(!filename.substr(filename.length()-4, filename.length()).compare(".ies")) {
-                            cout << entry->d_name << endl;
-                            string s(entry->d_name);
-                            IES::IES ies((argv[1] + ("/" + s)).c_str());
-                            rmdir("output/");
-                            mkdir("output/");
-                            ies.toSQL("output/" + string(entry->d_name) + ".sql", argv[2]);
-                            ies.toXML("ies/" + string(entry->d_name).substr(0, string(entry->d_name).length() - 4) + ".xml");
-                        }
-                        else cout << "ignoring " << filename << endl;
-                    }
-                    else
-                    {
-                        cout << "ignoring non regular file " << entry->d_name << endl;
-                    }
-                    entry = readdir(dir);
-
-                }
-
-                closedir(dir);
-            }
-            else {
-                cout << "can't open dir :";
-                perror("");
-            }
+            parseDirectoryRec(argv[1]);
         }
         else if(S_ISREG(buf.st_mode))
         {
@@ -61,9 +80,6 @@ int main(int argc, char * argv[]) {
     {
         cout << "Usage : " << argv[0] << " file.ies" <<" tablename" <<endl;
     }
-
-    cout << Database::getAssociatedTable("test");
-
 
     return 0;
 }
